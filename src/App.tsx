b2a8678ad31b9a445ed85643f0b13b9472b3b486
danielazevedocb <daniel.santoscb@gmail.com>
2025-09-10 17:27:27 -0300

    criando meu primeiro componente react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export function App() {
+  return (
+    <div>
+      <h1>Olá mundo(dp App)</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deserunt
+        ab rem qui iste modi! Accusantium accusamus eius ex, iste necessitatibus
+        consequuntur perspiciatis assumenda rem, adipisci dolorum nulla incidunt
+        possimus!
+      </p>
+    </div>
+  );
+}
