46286bdfb2503850ae9a49ddc521cb2d547b93c6
danielazevedocb <daniel.santoscb@gmail.com>
2025-09-10 22:21:42 -0300

    importando e usando arquivos de CSS globaiscriando

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { Heading } from './components/Heading';
+import './styles/global.css';
+import './styles/theme.css';
+
 export function App() {
   return (
-    <div>
-      <h1>Olá mundo(dp App)</h1>
+    <>
+      <>
+        <Heading />
+      </>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deserunt
         ab rem qui iste modi! Accusantium accusamus eius ex, iste necessitatibus
         consequuntur perspiciatis assumenda rem, adipisci dolorum nulla incidunt
         possimus!
       </p>
-    </div>
+    </>
   );
 }
